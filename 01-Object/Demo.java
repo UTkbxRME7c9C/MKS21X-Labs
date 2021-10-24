6444da9947fe0aa99f415ce7214cadf56952d2d2
John Doe <rhossain30@stuy.edu>
2021-10-24 01:30:04 -0400

    finished distance() inside Demo

---
+++ 01-Object/Demo.java
@@ -0,0 +1,14 @@
+public class Demo{
+	public static double distance(Point a, Point b){
+		return Math.sqrt(Math.pow(a.getX()-b.getX(),2)+Math.pow(a.getY()-b.getY(),2));
+
+	}
+
+    public static void main(String[]args){
+        Point P1 = new Point(1,2);
+        Point P2 = new Point(3,4);
+
+	System.out.println(distance(P1,P2));
+        System.out.println(  P1.distanceTo(P2));
+    }
+ } 
