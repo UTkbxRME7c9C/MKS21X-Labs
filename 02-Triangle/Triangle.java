8259dce658f8b85f8a06997c340abf91079ee1f2
John Doe <rhossain30@stuy.edu>
2021-10-25 18:53:41 -0400

    setup Triangle.java and toString

--- 02-Triangle/Triangle.java
+++ 02-Triangle/Triangle.java
@@ -1,14 +1,27 @@
 public class Triangle{
-	public Point getVertex(int ver){
-
+	private Point v1,v2,v3;
+	public Triangle(Point A, Point B, Point C){
+		v1 = A;
+		v2 = B;
+		v3 = C;
 	}
+	public  Triangle(double AX, double AY, double BX, double BY, double CX, double CY){
+		v1 = new Point(AX, AY);
+		v2 = new Point(BX, BY);
+		v3 = new Point(CX, CY);
+	}
+
 	public void setVertex(int ver, Point a){
 
 	}
 	public String toString(){
-
+		return "{"+v1.toString()+", "+v2.toString() + ", " + v3.toString()+"}";
 	}
-	public double getPerimeter(){
 
+	public static void main(String[]args) {
+		Triangle T1 = new Triangle(1,2,3,4,6,3);
+		Triangle T2 = new Triangle(new Point(1,2),new Point(3,4),new Point(6,3));
+		System.out.println(T1.toString());
+		System.out.println(T2.toString());
 	}
 }
