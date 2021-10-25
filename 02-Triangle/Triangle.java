300c1d0cbbc97457e2f1eb494e3a92c37fc25ca8
John Doe <rhossain30@stuy.edu>
2021-10-25 17:44:25 -0400

    init files

---
+++ 02-Triangle/Triangle.java
@@ -0,0 +1,14 @@
+public class Triangle{
+	public Point getVertex(int ver){
+
+	}
+	public void setVertex(int ver, Point a){
+
+	}
+	public String toString(){
+
+	}
+	public double getPerimeter(){
+
+	}
+}
