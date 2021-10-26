2709dae38207bfbab03ddbe81b3f742f341ff3b9
John Doe <rhossain30@stuy.edu>
2021-10-26 19:55:35 -0400

    added classify and area

--- 02-Triangle/Triangle.java
+++ 02-Triangle/Triangle.java
@@ -29,6 +29,24 @@ public class Triangle{
 	public double getPerimeter(){
 		return v1.distanceTo(v2) + v2.distanceTo(v3) + v3.distanceTo(v1);
 	}
+	public boolean equals(Triangle other){
+		return (v1.equals(other.v1) && v2.equals(other.v2)) && v3.equals(other.v3) ;
+	}
+	public String classify(){
+		double a= v1.distanceTo(v2);
+		double b= v2.distanceTo(v3);
+		double c= v3.distanceTo(v1);
+		if (closeEnough(a,b) && closeEnough(b,c)){
+			return "equilateral";
+		} else if ((closeEnough(a,b) || closeEnough(b,c)) || closeEnough(a,c) ){
+			return "isosceles";
+		}
+		return "scalene";
+	}
+	public double area(){
+		double s = (v1.distanceTo(v2) + v2.distanceTo(v3) + v3.distanceTo(v1))/2;
+		return Math.sqrt(s*(s-v1.distanceTo(v2))*(s-v2.distanceTo(v3))*(s-v3.distanceTo(v1)));
+	}
 
 	public static boolean closeEnough(double a, double b){
 		double clo = (Math.abs((a-b)/b))*100;
@@ -38,6 +56,7 @@ public class Triangle{
 		Triangle T1 = new Triangle(1,2,3,4,6,3);
 		Triangle T2 = new Triangle(new Point(1*Math.sqrt(4),2),new Point(3*Math.sqrt(4),4),new Point(6*Math.sqrt(3),3*Math.sqrt(2)));
 		Triangle T3 = new Triangle(2,4*Math.sqrt(6),8,10*Math.sqrt(12),1,1);
+		Triangle T4 = new Triangle(1,2,3,4,6,3);
 		System.out.println(T1.toString());
 		System.out.println(T2.getPerimeter());
 		System.out.println(T3.getVertex(1).toString());
@@ -47,5 +66,11 @@ public class Triangle{
 		Point P1 = new Point(0.1234567890,0.1234567899);
 		System.out.println(closeEnough(0.00000, 0.0000));
 		System.out.println(P1.equals(new Point(0.001, 0.001)));
+		System.out.println(T1.equals(T4));	
+		System.out.println(T1.area());
+		Triangle T5 = new Triangle(1,1,2,2,3,1);
+		Triangle T6 = new Triangle(0,0,3,3*Math.sqrt(3),6,0);
+	       	System.out.println(T5.classify());	
+	       	System.out.println(T6.classify());	
 	}
 }
