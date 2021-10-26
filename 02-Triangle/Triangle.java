62d7991b6c8707c6889ff0b4a141d69877b05a4e
John Doe <rhossain30@stuy.edu>
2021-10-25 20:18:27 -0400

    added getVertex and setVertex

--- 02-Triangle/Triangle.java
+++ 02-Triangle/Triangle.java
@@ -12,21 +12,34 @@ public class Triangle{
 	}
 
 	public void setVertex(int ver, Point a){
-
+		if (ver == 1)
+			v1 = a;
+		if (ver == 2)
+			v2 = a;
+		if (ver == 3)
+			v3 = a;
+	}
+	public Point getVertex(int ver){
+		Point[] pts = {v1,v2,v3};
+		return pts[ver-1];
 	}
 	public String toString(){
 		return "{"+v1.toString()+", "+v2.toString() + ", " + v3.toString()+"}";
+	}
+	public double getPerimeter(){
+		return v1.distanceTo(v2) + v2.distanceTo(v3) + v3.distanceTo(v1);
 	}
 
 	public static void main(String[]args) {
 		Triangle T1 = new Triangle(1,2,3,4,6,3);
 		Triangle T2 = new Triangle(new Point(1*Math.sqrt(4),2),new Point(3*Math.sqrt(4),4),new Point(6*Math.sqrt(3),3*Math.sqrt(2)));
+		Triangle T3 = new Triangle(2,4*Math.sqrt(6),8,10*Math.sqrt(12),1,1);
 		System.out.println(T1.toString());
-		System.out.println(T2.toString());
-		System.out.println(T1.getPerimeter());
 		System.out.println(T2.getPerimeter());
-	}
-	public double getPerimeter(){
-		return v1.distanceTo(v2) + v2.distanceTo(v3) + v3.distanceTo(v1);
+		System.out.println(T3.getVertex(1).toString());
+		T3.setVertex(1, new Point(5,5));
+		System.out.println(T3.getVertex(1).toString());
+		System.out.println(T3.toString());
+
 	}
 }
