6bd13805930cc04cb023a67f3d5ca30eda050e16
John Doe <rhossain30@stuy.edu>
2021-11-02 19:56:02 -0400

    cw

---
+++ Classwork/Tester.java
@@ -0,0 +1,8 @@
+public class Tester{
+  public static void main(String[] args){
+    Animal sd = new Animal("Loud", 16, "jeffery");
+    sd.speak();
+    Bird hhhh = new Bird("Loud", 16, "jeffery", 17.6, "black");
+    hhhh.speak();
+  }
+}
