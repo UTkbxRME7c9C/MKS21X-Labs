deb403dbe569ef33b60de988d7b8bb01249c1a55
John Doe <rhossain30@stuy.edu>
2021-11-02 23:57:59 -0400

    a

--- Classwork/2/Tester.java
+++
@@ -1,8 +0,0 @@
-public class Tester{
-  public static void main(String[] args){
-    Animal sd = new Animal("Loud", 16, "jeffery");
-    sd.speak();
-    Bird hhhh = new Bird("Loud", 16, "jeffery", 17.6, "black");
-    hhhh.speak();
-  }
-}
