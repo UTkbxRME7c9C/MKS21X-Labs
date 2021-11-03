344e088219845d9cab23e627cd933bfd77af3e0e
John Doe <rhossain30@stuy.edu>
2021-11-03 18:28:53 -0400

    added the compare and equals function

--- 03-GetReal/RealNumber.java
+++ 03-GetReal/RealNumber.java
@@ -28,4 +28,21 @@
     public String toString(){
       return ""+value;
     }
+    
+    public int compareTo(RealNumber other){
+      double minuss = value - other.value;
+      if (minuss < 0){
+        return -1;
+      }else if (minuss > 0){
+        return 1;
+      }
+      return 0;
+    }
+
+    public boolean equals(RealNumber other){
+      // maybe include closeEnough code?
+      if (value == other.value)
+        return true;
+      return false;
+    }
   }
--- 03-GetReal/Tester.java
+++ 03-GetReal/Tester.java
@@ -1,13 +1,17 @@
 public class Tester{
     public static void main(String[] args){
-        RealNumber now = new RealNumber(1.2345);
-        RealNumber wow = new RealNumber(55.5);
+        RealNumber now = new RealNumber(56.2);
+        RealNumber wow = new RealNumber(56.2);
         System.out.println(now.getValue());
         System.out.println(wow.getValue());
         System.out.println(wow.subtract(now));
         System.out.println(wow.divide(now));
         System.out.println(wow.multiply(now));
         System.out.println(wow.add(now));
+        System.out.println(wow.compareTo(now));
+        System.out.println(wow.equals(now));
+
+        /*
         RationalNumber naw = new RationalNumber(1, 2);
         RationalNumber waw = new RationalNumber(3, 4);
         System.out.println("rational " + waw.getNumerator());
@@ -19,7 +23,7 @@ public class Tester{
         System.out.println(waw.divide(naw).toString());
         System.out.println(waw.add(naw).toString());
         System.out.println(naw.subtract(waw).toString());
-
+        */
 
 
 
