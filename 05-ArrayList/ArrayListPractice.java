c2e08adef8c61bfc9f64772a7da6e1989fb79e35
John Doe <rhossain30@stuy.edu>
2021-11-17 21:20:27 -0500

    added arraylist functions

--- 05-ArrayList/ArrayListPractice.java
+++ 05-ArrayList/ArrayListPractice.java
@@ -1,28 +1,64 @@
 import java.util.ArrayList;
 public class ArrayListPractice{
   public static ArrayList<String> replaceEmpty( ArrayList<String> data){
-    ArrayList<String> e = new ArrayList<String>(data.size());
+    ArrayList<String> e = new ArrayList<String>();
     for (int i=0;i<data.size();i++){
         if (data.get(i).equals("")){
-          e.set(i,"Empty")
+          e.add("Empty");
         }else{
-          e.set(i, data.get(i));
+          e.add(data.get(i));
         }
     }
     return e;
   }
 
+  public static ArrayList<String> newRandomArray(int len){
+    ArrayList<String> e = new ArrayList<String>();
+    for (int i = 0;i<len;i++){
+      e.add(""+ (int)(Math.random()*100));
+    }
+    return e;
+  }
+
   public static ArrayList<String> makeReversedList( ArrayList<String> data){
-    ArrayList<String> e = new ArrayList<String>(data.size());
+    ArrayList<String> e = new ArrayList<String>();
     for(int i=0;i<data.size();i++){
-      e.set(data.size()-i, data.get(i));
+      e.add( data.get(data.size()-1-i));
     }
     return e;
   }
 
   public static ArrayList<String> mixLists( ArrayList<String> a,  ArrayList<String> b){
-    //return a new ArrayList that has all values of a and b in alternating order that is:
-    //a[0], b[0], a[1], b[1]...
-    //when one list is longer than the other, just append the remaining values to the end.
+    int maxi = Math.max(a.size(), b.size());
+    int mini = Math.min(a.size(), b.size());
+    ArrayList<String> e = new ArrayList<String>();  
+    for (int i = 0;i<maxi; i++){
+      if (i < mini){
+        e.add(a.get(i));
+        e.add(b.get(i));
+      } else if (a.size() == maxi){
+        e.add(a.get(i));
+      } else {
+        e.add(b.get(i));
+      }
+    }
+    return e;
+  }
+  public static void main(String[] args){
+    ArrayList<String> arr1 = newRandomArray(4); 
+    ArrayList<String> arr2 = newRandomArray(200000);
+    ArrayList<String> arr3 = newRandomArray(200000);
+    /*
+    System.out.println(arr1 + " " + arr2);
+    System.out.println(mixLists(arr1,arr2));
+    System.out.println(makeReversedList(arr1));
+    arr1.set(0,"");
+    System.out.println(replaceEmpty(arr1));
+    */
+    arr1 = makeReversedList(arr3);
+    arr1 = mixLists(arr1,arr2);
+    System.out.println(arr1);
+
+
   }
 }
