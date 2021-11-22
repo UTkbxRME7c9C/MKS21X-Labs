dd48b5c3f580dec8f88158135d111f712ef97cc7
Rafayet <rhossain30@stuy.edu>
2021-11-22 11:01:57 -0500

    z

--- 06-Advent/Triangles.java
+++ 06-Advent/Triangles.java
@@ -1,1 +1,20 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Triangles{
+  public static void Part1(String filename){
+    File file = new File(filename);
+    Scanner input = new Scanner(file);
+    while(input.hasNextLine()){
+      int a = input.nextInt();
+      int b = input.nextInt();
+      int c = input.nextInt();
+    }
+  }
+  public static void main(String[] args){
+
+  }
+}
+
+//52
