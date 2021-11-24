36ab949051e8dd80358ee5cab2abdd7f035f44ef
Rafayet <rhossain30@stuy.edu>
2021-11-24 10:40:49 -0500

    finish wrapper

--- 06-Advent/Wrapper.java
+++ 06-Advent/Wrapper.java
@@ -19,7 +19,20 @@ public class Wrapper{
                 input.close();
                 System.out.println(total);
             }else if (args[1].equals("2")){
-
+              File file = new File(args[0]);
+              Scanner input = new Scanner(file);
+              int total = 0;
+              while (input.hasNextLine()){
+                    String[] line = (input.nextLine().split("x"));
+                    int l = Integer.parseInt(line[0]);
+                    int w = Integer.parseInt(line[1]);
+                    int h = Integer.parseInt(line[2]);
+                    int least = Math.min(Math.min(l,w),h);
+                    int secleast = ((l+w)+h) - (least + (Math.max(Math.max(l,w), h)));
+                    total += ((l*w)*h) + (2*least) + (2*secleast);
+              }
+              input.close();
+              System.out.println(total);
             }else{
                 throw new NoSuchFieldError("not a valid method");
             }
