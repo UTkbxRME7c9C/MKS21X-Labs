23b7870a3795c72be819956deaeeb141c13a8d93
Rafayet <rhossain30@stuy.edu>
2021-11-29 11:01:54 -0500

    a a a

---
+++ 07-BinarySearch/BinarySearch.java
@@ -0,0 +1,15 @@
+public class BinarySearch{
+  public static int binarySearch(int[] data, int target){
+    boolean go = true;
+    int first = 0;
+    int last = data.length-1;
+    while (go){
+      if ((first+last)/2 == target){
+      }
+    }
+    return -1;
+  }
+
+  public static void main(String[] args){
+  }
+}
